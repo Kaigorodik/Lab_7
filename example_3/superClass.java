674a23584b2722cb4677f522e22730f7f9ce8a7a
Kaigorodik <70796797+Kaigorodik@users.noreply.github.com>
2020-11-21 14:52:13 +0500

    Create superClass.java

---
+++ example_3/superClass.java
@@ -0,0 +1,18 @@
+package lab_7_example_07_03;
+
+public class superClass {
+	
+	public int superClass;
+	
+	public superClass(int superClass) {
+        this.superClass = superClass;
+    }
+	
+	 public void setParameter(int superClass) {
+	        this.superClass = superClass;
+	 }
+	 
+	 @Override
+	    public String toString() {
+	        return "superClass{" + "superClass=" + superClass +'}';
+}}
